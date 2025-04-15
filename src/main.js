0be790aa66e55a19c1837b9a0b92454bd23267fa
Obed-Olavarria <ing.olavarria.mannager@gmail.com>
2025-04-15 16:14:55 -0600

    new
    new

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue';
-import './style.css';
-import App from './App.vue';
-import router from './router'; // Importa Vue Router
-
-// Crea una sola instancia de la app y usa Vue Router
-createApp(App).use(router).mount('#app');
